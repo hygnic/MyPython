a9e2886bd9930bb2444069e3d4c1bb89faa32894
hygnic <hygnic@outlook.com>
2021-01-04 00:39:40 +0800

    0104

---
+++ 2020/Advanced_Python/序列_具名元组.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# ---------------------------------------------------------------------------
+# Author: LiaoChenchen
+# Created on: 2021/1/1 23:50
+# Reference:
+"""
+Description:
+Usage:
+"""
+# ---------------------------------------------------------------------------
+from collections import namedtuple
+
+Card = namedtuple("Card", "first second")
+new_card = Card(19, 20)
+print("spam is this a hterr".split())
